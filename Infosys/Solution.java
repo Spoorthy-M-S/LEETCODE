ef655b43d5abbda0917e67357380e7ad4fd54497
Spoorthy_M_S <spoorthyms462003@gmail.com>
2024-06-09 03:56:24 +0900

    add

--- Infosys/Solution.java
+++ Infosys/Solution.java
@@ -69,3 +69,32 @@ public class Solution {
         System.out.println(maxSum(N, X, Y, Z, A, B));
     }
 }
+/*
+ * You are given three integers X,Y and Z and two arrays A and B both of length N. You are also given an integer sum which is initially equal to 0.
+
+You have perform N operations and in each ith operation you must do only one of the following:
+
+1. Subtract B[i] from sum.
+
+2. Decrease both of X and Y by 1, then add A[i] X*Y Z to sum.
+
+3. Decrease both of Y and Z by 1, then add A[i] X*Y Z to sum.
+
+However, after each operation, X,Y and Z must all remain greater than or equal to 0.
+
+Find the maximum sum you can obtain after performing all operations. Since answer can be large, return it modulo 10 ^ 9 + 7
+
+Input Format
+
+1. The first line contains an integer, N, denoting the number of operations.
+
+2. The next line contains an integer, X.
+
+3. The next line contains an integer, Y.
+
+4. The next line contains an integer, Z.
+
+5. Each line i of the N subsequent lines (where 1 <= i <= N contains an integer describing A[i].
+
+6. Each line i of the N subsequent lines (where 1 <= i <= N ) contains an integer describing
+ */
